79ad4ff26075c0e2132c4d3609ad00cb418fbd7c
Iulian Radu <iulian.radu@gmx.at>
2023-12-10 20:51:35 +0100

    Updated packages and storybook

--- storybook/stories.tsx
+++ storybook/stories.tsx
@@ -1,6 +1,6 @@
+import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import ResponsiveCssMaterialUi from '../src/ResponsiveCssMaterialUi';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 const styles: { [key: string]: React.CSSProperties } = {
   text: {
@@ -15,38 +15,38 @@ const md: React.CSSProperties = { backgroundColor: '#ffcc00' };
 const lg: React.CSSProperties = { backgroundColor: '#336699' };
 const xl: React.CSSProperties = { backgroundColor: '#003366' };
 
-export default {
+const meta: Meta<typeof ResponsiveCssMaterialUi> = {
   title: 'ResponsiveCssMaterialUi',
   component: ResponsiveCssMaterialUi,
-} as ComponentMeta<typeof ResponsiveCssMaterialUi>;
+} as Meta<typeof ResponsiveCssMaterialUi>;
+export default meta;
+type Story = StoryObj<typeof ResponsiveCssMaterialUi>;
 
-export const WithDebugDisabled: ComponentStory<typeof ResponsiveCssMaterialUi> =
-  () => (
+export const WithDebugDisabled = () => (
+  <div>
+    <ResponsiveCssMaterialUi xs={xs} md={md} sm={sm} lg={lg} xl={xl}>
+      <div style={styles.text}>Text</div>
+    </ResponsiveCssMaterialUi>
     <div>
-      <ResponsiveCssMaterialUi xs={xs} md={md} sm={sm} lg={lg} xl={xl}>
-        <div style={styles.text}>Text</div>
-      </ResponsiveCssMaterialUi>
-      <div>
-        Please resize the page width to see different colors for different sizes
-      </div>
+      Please resize the page width to see different colors for different sizes
     </div>
-  );
+  </div>
+);
 
-export const WithDebugEnabled: ComponentStory<typeof ResponsiveCssMaterialUi> =
-  () => (
+export const WithDebugEnabled = () => (
+  <div>
+    <ResponsiveCssMaterialUi
+      xs={xs}
+      md={md}
+      sm={sm}
+      lg={lg}
+      xl={xl}
+      debug={true}
+    >
+      <div style={styles.text}>Text</div>
+    </ResponsiveCssMaterialUi>
     <div>
-      <ResponsiveCssMaterialUi
-        xs={xs}
-        md={md}
-        sm={sm}
-        lg={lg}
-        xl={xl}
-        debug={true}
-      >
-        <div style={styles.text}>Text</div>
-      </ResponsiveCssMaterialUi>
-      <div>
-        Please resize the page width to see different colors for different sizes
-      </div>
+      Please resize the page width to see different colors for different sizes
     </div>
-  );
+  </div>
+);
